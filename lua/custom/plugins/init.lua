ec22ed9b0e8d25d71d53a4da8ef48603cef4df8b
Ernesto Hegi <ernesto.hegi@gmail.com>
2024-11-29 18:36:37 +0000

    removes custom plugin files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -55,4 +55,15 @@ return {
       }
     end,
   },
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {}
+    end,
+  },
 }
