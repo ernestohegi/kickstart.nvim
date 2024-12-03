5380e34b4f32951005485acbe2cf2ea03d9a08dc
Ernesto Hegi <ernesto.hegi@gmail.com>
2024-12-03 12:48:39 +0000

    adds spectre to search and replace

--- init.lua
+++ init.lua
@@ -112,6 +112,8 @@ vim.opt.number = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
+vim.o.autoread = true
+
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
@@ -1009,7 +1011,7 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-
+  'nvim-pack/nvim-spectre',
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
